41b6744eb85148da5fa835acd7304a481b878945
newgen <newgen@LPT2110.newgen.co.in>
2022-11-16 13:20:35 +0530

    git commit

---
+++ spring-boot-h2/src/main/java/com/example/demo/controller/EmployeeController.java
@@ -0,0 +1,59 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import javax.annotation.Generated;
+
+import org.apache.catalina.startup.ClassLoaderFactory.Repository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.dao.EmployeeRepository;
+import com.example.demo.model.Employee;
+
+@RestController
+public class EmployeeController {
+@Autowired
+private EmployeeRepository empRepository;
+@PostMapping("/saveEmployee")
+
+public String saveEmployee(@RequestBody Employee employee)
+{  try
+{   System.out.println("inside try");
+	empRepository.save(employee);
+	
+}
+catch(Exception e)
+{  System.out.println("catch");
+	e.printStackTrace();
+	System.out.println("message"+e.getMessage());
+}
+return "Employee Saved";
+}
+
+@GetMapping("/getAllEmployees")
+public List<Employee> getAll()
+{  System.out.println("getall");
+	try
+	{
+	return empRepository.findAll();
+	}
+	catch(Exception e)
+	{
+		e.printStackTrace();
+		System.out.println("e"+e.getMessage());
+	}
+	return empRepository.findAll();
+}
+
+@GetMapping("/getEmployee/{dept}")	
+public List<Employee> getEmployeesByDept(@PathVariable String dept)
+{
+	return empRepository.findById(dept);
+}
+
+}
---
+++ spring-boot-h2/src/main/java/com/example/demo/model/Employee.java
@@ -0,0 +1,24 @@
+package com.example.demo.model;
+
+import javax.annotation.Generated;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee {
+	@Id
+	@GeneratedValue
+	private int id;
+	private String name;
+	private String dept;
+	private double salary;
+
+}
